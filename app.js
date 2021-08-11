18773f52de006ca6a481d4f149abcc3164a7e048
momo427 <ferguson.tyra@gmail.com>
2021-08-11 08:10:22 -0400

    app.js blueprint

---
+++ app.js
@@ -0,0 +1,90 @@
+//quiz variables
+let question = [questions]
+let timer = document.querySelector('timer');
+let quizHeader = document.querySelector('quiz-header');
+let quizLoaded = document.querySelector('quiz');
+let quizTitle = document.querySelector('question-title');
+let multipleChoice = document.querySelector('multiple-choice');
+let answerResponse = document.querySelector('right-v-wrong');
+let startQuiz = document.querySelector('startquiz');
+let submitQuiz = document.querySelector('submit');
+
+//score variables
+let scoreList = document.querySelector('score-list');
+let claimVictory = document.querySelector('claimVictory')
+
+//questions array
+let questions = [
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+},
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    },
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    },
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    },
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    },
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    },
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    },
+    {
+        question: "",
+        multipleChoice: [],
+        answer:""
+
+    }
+]
+//timer function
+ function countdown (params) {
+     if (condition) {
+         
+     } else {
+         
+     }
+ }
+//score function
+function Tally (params) {
+    if (condition) {
+        
+    } else {
+        
+    }
+}
+
+//question function
+function questions (params) {
+    if (condition) {
+        
+    } else {
+        
+    }
+}
